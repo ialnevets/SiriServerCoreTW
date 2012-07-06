79fc1aedf72a4cdac7e8a297cf4845724829f1fd
ialnevets <steven851230@gmail.com>
2012-07-06 18:10:37 +0800

    HTTP協定

---
+++ httpClient.py
@@ -0,0 +1,27 @@
+from twisted.internet import threads, defer
+import urllib2
+import logging
+
+class AsyncOpenHttp(object):
+    def __init__(self, callback):
+        super(AsyncOpenHttp, self).__init__()
+        self.callback = callback
+    
+    def make_google_request(self, flac, requestId, dictation, language="de-DE", allowCurses=True):
+        d = threads.deferToThread(self.run, flac, requestId, dictation, language, allowCurses)
+        d.addCallback(self.callback, requestId, dictation)
+        d.addErrback(self.onError)
+        return d
+    
+    def onError(self, failure):
+        failure.trap(defer.CancelledError)
+        logging.getLogger().info("Google request canceled")
+        pass
+    def run(self, flac, requestId, dictation, language, allowCurses):
+        url = "https://www.google.com/speech-api/v1/recognize?xjerr=1&client=chromium&pfilter={0}&lang=zh-TW&maxresults=6".format(0 if allowCurses else 2)
+        req = urllib2.Request(url, data = flac, headers = {'Content-Type': 'audio/x-flac; rate=16000', 'User-Agent': 'Siri-Server'})
+        try:
+            body  = urllib2.urlopen(req, timeout=5).read()
+            return body
+        except:
+            return None
